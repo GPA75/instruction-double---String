adbc64f2f30283ea2027b907c31f9e1e3cc1d648
GPA75 <47542080+GPA75@users.noreply.github.com>
2019-03-06 12:22:54 -0300

    Object Big Decimal
    Using one math-BigDecimal for values accurate.

--- src/amount/Amount.java
+++ src/amount/Amount.java
@@ -1,25 +1,31 @@
 package amount;
 
+import java.text.NumberFormat;
+import java.math.*;
+import java.util.*;
+
 // compound interest
 public class Amount {
     
     public static void main(String[] args) {
-        
-        double amount; // Determination of amount
-        double principal = 10000.00; // Determination of investment
-        double rate = 0.05; // Dtermination of rate
-        
+       
+       // main amount initial  
+       BigDecimal principal = BigDecimal.valueOf(1000); // Determination of investment
+       BigDecimal rate = BigDecimal.valueOf(0.05); // Dtermination of rate
+       
         // Title
-        System.out.printf("%s%25s %n","Year", "Amount on deposit");
+       System.out.printf("%s%25s%n", "Year", "Amount on deposit");
         
         // Method "for", Instrution of loop
-        for (int year = 1; year <= 20; ++year) {
+       for (int year = 1; year <= 20; ++year) {
             
-            // compound interest formula
-            amount = principal * Math.pow(1.0 + rate, year);
+            // compound interest formula with BigDecimal
+            BigDecimal amount = 
+              principal.multiply(rate.add(BigDecimal.ONE).pow(year));
         
         // display the amount
-        System.out.printf("%4d%,25.2f%n", year, amount);
+        System.out.printf("  %02d%25s%n", year, 
+             NumberFormat.getCurrencyInstance(Locale.US).format(amount));
         }
     }
-}
+} // End of class Amount
